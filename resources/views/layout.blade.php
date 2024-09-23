673ceb6a620f84fe318fcfee99b899c0d5c90b8d
dwiponcosuripto4 <dwiponcosuripto44@gmai.com>
2024-09-23 22:08:24 +0700

    tes4

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,15 +19,12 @@
 
 <body>
     {{-- Navbar --}}
-    <nav class="{{ request()->is('/') ? 'navbar-index' : 'navbar-default' }} navbar navbar-expand-lg shadow py-4">
-        <div class="container col-xxl-12 d-flex justify-content-between">
+    <nav class="navbar {{ request()->is('/') ? 'navbar-index' : 'navbar-default' }} navbar-expand-lg shadow py-4">
+        <div class="container-fluid d-flex justify-content-between">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="https://okutimurkab.go.id/wp-content/themes/okutimurkab/images/logo_horisontal.png"
-                    height="100" alt="" />
+                <img src="https://okutimurkab.go.id/wp-content/themes/okutimurkab/images/logo_horisontal.png" height="60" alt="" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -36,10 +33,9 @@
                         <a class="nav-link active text-white" aria-current="page" href="{{ url('/') }}">Beranda</a>
                     </li>
                     @foreach ($categories as $category)
-                        <li class="nav-item dropdown category-item">
-                            <a class="nav-link dropdown-toggle text-white ease-in-out transition-transform duration-300"
-                                href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {{ $category->title }}
+                    <li class="nav-item dropdown category-item">
+                        <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ $category->title }}
                                 <i class="bi bi-chevron-down transform transition-transform duration-300"></i>
                             </a>
                             <ul class="dropdown-menu">
@@ -78,14 +74,14 @@
                     @endforeach
                 </ul>
                 <!-- Admin Button -->
-            <div class="dropdown ms-auto">
-                <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                    </svg>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item" href="{{ url('/post/data') }}">Post Data</a></li>
+                <div class="dropdown ms-auto">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="{{ url('/post/data') }}">Post Data</a></li>
                     <li><a class="dropdown-item" href="{{ url('/category/data') }}">Category Data</a></li>
                     <li><a class="dropdown-item" href="{{ url('/headline/data') }}">Headline Data</a></li>
                     <li><a class="dropdown-item" href="{{ url('/data/index') }}">Add Data</a></li>
